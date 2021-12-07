c1db0911dd06060bb2c1023e3ea21c4d43870032
Sarcastic14 <95715059+Sarcastic14@users.noreply.github.com>
2021-12-07 17:06:44 -0300

    Update connect.js

--- connect.js
+++ connect.js
@@ -1,1 +1,32 @@
 
+const { WAConnection } = require('@adiwajshing/baileys');
+const fs = require('fs');
+
+const auth = './session.json'
+
+const client = new WAConnection()
+exports.client = client
+
+exports.connect = async() => {
+client.logger.level = 'warn'
+client.version = [2, 2143, 3]
+
+client.on('qr', () => {
+console.log('Escanee el Qr Code')
+})
+
+fs.existsSync(auth) && client.loadAuthInfo(auth)
+
+client.on('connecting', () => {
+console.log('Conectando')
+})
+
+client.on('open', () => {
+console.log('Conectado exitosamente')
+const authInfo = client.base64EncodedAuthInfo()
+fs.writeFileSync(auth, JSON.stringify(authInfo, null, '\t'))
+})
+
+await client.connect({timeoutMs: 30 * 1000})
+return
+}
