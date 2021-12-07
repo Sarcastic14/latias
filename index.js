2d56719164f8590a4550c5991ff01686d9a0e9d1
Sarcastic14 <95715059+Sarcastic14@users.noreply.github.com>
2021-12-07 17:04:00 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+const { WAConnection } = require('@adiwajshing/baileys');
+const fs = require('fs');
 
+const conn = require('./connect');
+const connect = require('./connect');
+
+conn.connect()
+const client = conn.client
+
+client.on('chat-update', async (mek) => {
+try{
+if (!mek.hasNewMessage) return
+if (!mek.messages) return
+if (mek.key && mek.key.remoteJid == 'status@broadcast') return
+
+mek = mek.messages.all()[0]
+
+} catch (e) {
+console.log(e)
+}
